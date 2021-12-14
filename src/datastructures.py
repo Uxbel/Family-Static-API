e281cc54406908d16fb536fad94043594c8ad414
Uxbel <jorgebelda@indotem.com>
2021-12-14 19:11:47 +0000

    Carga ejercicio

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,18 +1,9 @@
-
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        # example list of members
         self._members = [{"id": 1, "first_name": "John", "age": 33, "lucky_numbers": [7,13,22]}, 
         {"id": 2,"first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
         {"id": 3,"first_name": "Jimmy", "age": 5, "lucky_numbers": [1]}]
